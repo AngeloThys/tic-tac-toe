70299db3fc4ac207f9ae64a1f8a005019de30e68
Angelo Thys <thys.angelo@gmail.com>
2023-10-04 18:30:07 +0100

    Fix markup and styles numbering

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 "use strict";
 
 const game = (function () {
-  return {
-
-  };
+  return {};
 })();
 
 const gameboard = (function () {
-    const gameboard = [];
+  const gameboard = [
+    ["x", "x", "o"],
+    ["o", "o", "x"],
+    ["x", "o", "x"],
+  ];
+  const display = function () {
+    console.log(this.gameboard);
+    for (let i = 0; i < this.gameboard.length; i++) {
+      for (let j = 0; j < this.gameboard[i].length; j++) {
+        const cell = document.querySelector(`.y-${i}.x-${j}`);
+        console.log(`.y-${i}.x-${j}`);
+        cell.textContent = this.gameboard[i][j];
+      }
+    }
+  };
   return {
     gameboard: gameboard,
+    display: display
   };
 })();
 
 function player(name, symbol) {
   return { name, symbol };
 }
+
+gameboard.display()
