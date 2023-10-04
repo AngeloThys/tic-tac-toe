f035e152a87838a6b523d135fd8058c5659e7ed6
Angelo Thys <thys.angelo@gmail.com>
2023-10-04 18:51:10 +0100

    Add display function to gameboard

--- script.js
+++ script.js
@@ -11,18 +11,25 @@ const gameboard = (function () {
     ["x", "o", "x"],
   ];
   const display = function () {
-    console.log(this.gameboard);
     for (let i = 0; i < this.gameboard.length; i++) {
       for (let j = 0; j < this.gameboard[i].length; j++) {
         const cell = document.querySelector(`.y-${i}.x-${j}`);
-        console.log(`.y-${i}.x-${j}`);
-        cell.textContent = this.gameboard[i][j];
+        switch (this.gameboard[i][j]) {
+          case "x":
+            cell.innerHTML =
+              "<span class='close material-symbols-outlined'>close</span>";
+            break;
+          case "o":
+            cell.innerHTML =
+              "<span class='circle material-symbols-outlined'>circle</span>";
+            break;
+        }
       }
     }
   };
   return {
     gameboard: gameboard,
-    display: display
+    display: display,
   };
 })();
 
@@ -30,4 +37,4 @@ function player(name, symbol) {
   return { name, symbol };
 }
 
-gameboard.display()
+gameboard.display();
