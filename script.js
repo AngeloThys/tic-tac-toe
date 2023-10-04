556776a916eaca100107c33c14d2fcf9e4fdd9e3
Angelo Thys <thys.angelo@gmail.com>
2023-10-04 17:48:58 +0100

    Add styles

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const game = (function () {
+  return {
+
+  };
+})();
+
+const gameboard = (function () {
+    const gameboard = [];
+  return {
+    gameboard: gameboard,
+  };
+})();
+
+function player(name, symbol) {
+  return { name, symbol };
+}
