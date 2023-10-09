05fcb10514fd32e025b098a765e7ea8dee020478
Angelo Thys <thys.angelo@gmail.com>
2023-10-09 18:27:16 +0100

    Add checkThree function that checks for three in a row Add getPlayerNames function that obtains the players names from form Add createPlayers function that creates players Add playGame function that allows the game to be played and replayed Add markup and styles for multiple modals

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ const cell = function () {
   return { setToken, getToken };
 };
 
-const player = function () {
-  const username = "";
-  const token = "";
+const player = function (username, token) {
   const setUsername = (newUsername) => (username = newUsername);
   const getUserName = () => username;
   const setToken = (newToken) => (token = newToken);
@@ -52,14 +50,68 @@ const gameboard = (function () {
   const setCell = function (player, e) {
     const row = e.target.dataset.row;
     const column = e.target.dataset.column;
-
     gameboard[row][column].setToken(player.getToken());
   };
+  // returns the token of the player with 3 in a row.
+  const checkThree = function () {
+    let cellOne = "";
+    let cellTwo = "";
+    let cellThree = "";
+    // check each row
+    for (let row = 0; row < gameboard.length; row++) {
+      cellOne = gameboard[row][0];
+      cellTwo = gameboard[row][1];
+      cellThree = gameboard[row][2];
+      if (
+        cellOne.getToken() === cellTwo.getToken() &&
+        cellOne.getToken() === cellThree.getToken()
+      ) {
+        return cellOne.getToken();
+      }
+    }
+    // check each column
+    for (let row = 0; row < gameboard.length; row++) {
+      for (let column = 0; column < gameboard[row]; column++) {
+        cellOne = gameboard[0][column];
+        cellOne = gameboard[1][column];
+        cellOne = gameboard[2][column];
+        if (
+          cellOne.getToken() === cellTwo.getToken() &&
+          cellOne.getToken() === cellThree.getToken()
+        ) {
+          return cellOne.getToken();
+        }
+      }
+    }
+    // check both diagonals
+    cellOne = gameboard[0][0];
+    cellTwo = gameboard[1][1];
+    cellThree = gameboard[2][2];
+    if (
+      cellOne.getToken() === cellTwo.getToken() &&
+      cellOne.getToken() === cellThree.getToken()
+    ) {
+      return cellOne.getToken();
+    }
+    cellOne = gameboard[0][2];
+    cellTwo = gameboard[1][1];
+    cellThree = gameboard[2][0];
+    if (
+      cellOne.getToken() === cellTwo.getToken() &&
+      cellOne.getToken() === cellThree.getToken()
+    ) {
+      return cellOne.getToken();
+    }
+  };
 
-  return { display, setCell };
+  const checkFull = function () {};
+
+  return { display, setCell, checkThree, checkFull };
 })();
 
 const game = (function () {
+  let playerOne, playerTwo;
+
   const newGameButton = document.querySelector(".new-game");
   const dialog = document.querySelector("dialog");
   newGameButton.addEventListener("click", () => dialog.showModal());
@@ -70,10 +122,79 @@ const game = (function () {
     }
   });
 
-  const gameDiv = document.querySelector(".game");
-  gameDiv.addEventListener("click", (e) => {
-    gameboard.setCell("player-1", e); // Modify player-1 to hold the current player
+  const getPlayerNames = function () {
+    const playerNameOne = document.querySelector("#username-1").value;
+    const playerNameTwo = document.querySelector("#username-2").value;
+
+    return [playerNameOne, playerNameTwo];
+  };
+
+  const createPlayers = function (playerNameOne, playerNameTwo) {
+    playerOne = player(playerNameOne, "x");
+    playerTwo = player(playerNameTwo, "o");
+  };
+
+  const playGame = function (player) {
+    let winner = "";
+    const gameDiv = document.querySelector(".game");
+    gameDiv.addEventListener("click", (e) => {
+      // we check if the cell is valid
+      if (e.target.classList[0] === "cell") {
+        // Changes the player each turn on correct cell click
+        player = player === playerTwo ? playerOne : playerTwo;
+        gameboard.setCell(player, e);
+        gameboard.display();
+      } else if (
+        e.target.classList[0] === "circle" ||
+        e.target.classList[0] === "close"
+      ) {
+        const dialog = document.querySelector(".new-cell");
+        dialog.showModal();
+        dialog.style.opacity = 0;
+        dialog.addEventListener("transitionend", () => {
+          dialog.close();
+          dialog.style.opacity = 1;
+        });
+      }
+      // checks if there is a three in a row: token type wins.
+      const winnerToken = gameboard.checkThree();
+      if (winnerToken) {
+        const winnerH1 = document.querySelector(".winner");
+        switch (winnerToken) {
+          case "x":
+            winner = playerOne.getUserName();
+            winnerH1.style.color = "var(--primary-blue)";
+            break;
+          case "o":
+            winner = playerTwo.getUserName();
+            winnerH1.style.color = "var(--primary-red)";
+            break;
+        }
+        winnerH1.textContent = winner;
+        const dialog = document.querySelector(".play-again");
+        dialog.showModal();
+
+        const playAgainButton = document.querySelector(".play-again .play");
+        playAgainButton.addEventListener("click", () => {
+          // add score to winner
+          // clear gameboard
+          dialog.close();
+          let player = playerOne;
+          playGame(player);
+        });
+      }
+      // checks if the board is full: tie.
+      gameboard.checkFull();
+    });
+  };
+
+  const playButton = document.querySelector(".play-game .play");
+  playButton.addEventListener("click", () => {
+    createPlayers(...getPlayerNames());
+    dialog.close();
+    let player = playerTwo;
+    playGame(player);
   });
 
-  gameboard.display();
+  return {};
 })();
